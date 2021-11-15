29676e110a7c9baf592f379612f75d6b4596f2fd
ksandrill <edgyestlord@gmail.com>
2021-11-15 18:06:53 +0700

    fix

--- main.py
+++ main.py
@@ -1,32 +1,21 @@
 from timeit import timeit
 
+from sift_algo_phases.sift import sift_calculation
 import cv2
 import numpy as np
-import gauss_filter
-from sift import get_dog, get_keypoints
 
 
-def sift(picture: np.ndarray):
-    dog, gaussian_pyramid = get_dog(picture, octaves_number=8)
-    keypoints = get_keypoints(dog, gaussian_pyramid)
-    return keypoints
-
-
-def main() -> None:
-    picture = cv2.imread('pictures/robopenguin.bmp')
-    orig_image = picture.copy()
-    picture = gauss_filter.gaussian_blur(picture, (5, 5), 2)
-    keypoints = sift(picture)
-    print('keypoint_number: ', len(keypoints))
-    for keypoint in keypoints:
-        x = keypoint.pos_x
-        y = keypoint.pos_y
-        print(keypoint)
-        # print(x)
-        # print(y)
-        orig_image[y][x] = [0, 0, 255]
-    cv2.imshow('dafaq', orig_image)
-    cv2.waitKey(0)
+def main():
+    picture1 = cv2.imread('pictures/robopenguin.bmp', 0)
+    picture2 = cv2.imread('pictures/rotate_robopenguin.bmp', 0)
+    keypoints1, descriptors1 = sift_calculation(picture1)
+    keypoints2, descriptors2 = sift_calculation(picture2)
+    # h1, w1 = picture1.shape
+    # h2, w2 = picture2.shape
+    # h_max = max(h1, h2)
+    # w_max = max(w1, w2)
+    # cv2.waitKey(0)
+    #
 
 
 if __name__ == '__main__':
